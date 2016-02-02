ec216b57a1dd9d0e6e6460d6417c2504396aa0d0
amine <amine.benyessef@gmail.com>
2016-02-02 15:58:59 +0100

    Ajout à ctrl.js d'un controle d'identité des 2 mots de passe

--- ctrl.js
+++ ctrl.js
@@ -1,4 +1,5 @@
 $(function(){
+//Controle de longueur du MdP
   $('#pswd1').on('blur',function(){
     if($('#pswd1').val().length<6){
          $('#pswd_msg1').css('display','block');
@@ -7,5 +8,15 @@ $(function(){
         $('#pswd_msg1').css('display','none');
         }
       });
+//Controle d'identité des 2 MdP
+ $('#pswd1').on('blur',function(){
+    if($('#pswd2').val()!=$('#pswd1').val()){
+         $('#pswd_msg2').css('display','block');
+        }
+    else {
+        $('#pswd_msg2').css('display','none');
+        }
+      });
+
 });
 
